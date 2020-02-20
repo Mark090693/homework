dfad797c18d2b9db36e2005656a492fad24431ba
Mark090693 <alinashpitalnikova1993>
2020-02-20 15:10:39 +0300

    Complite second home work

---
+++ HW2/src/by/yakovlev/Time.java
@@ -0,0 +1,27 @@
+package by.yakovlev;
+
+class Main {
+
+    public static void main(String[] args) {
+        long s = 624298445L;
+        long sec;
+        long m;
+        long min, h;
+        long hour , d ;
+        long days, w;
+        long week;
+
+        sec = s % 60;
+        m = (s - sec) / 60;
+        min = m % 60;
+        h = (m - min) / 60;
+        hour = h % 24;
+        d = (h - hour) / 24;
+        days = d % 7;
+        week = d - days;
+
+
+
+        System.out.println(week+ " недель " + days+ " дней " + hour + " часов " + min + " минут " + sec + "секунд");
+    }
+}
