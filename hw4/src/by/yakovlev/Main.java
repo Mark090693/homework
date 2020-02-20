79f4d42af5e05834acedc3e5b6fbd76f321366c6
Mark090693 <alinashpitalnikova1993>
2020-02-20 15:42:56 +0300

    Complite fourth home work

---
+++ hw4/src/by/yakovlev/Main.java
@@ -0,0 +1,14 @@
+package by.yakovlev;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a = 5;
+	int b = 8;
+	int r = 5;
+	if (r * 2 <= b)
+        System.out.println("Круглая фигура не покроет прямоугольную дырку");
+	else
+        System.out.println("Круглая фигура покроет прямоугольную дырку");
+    }
+}
