1571d5b35f86e47f8a169bc998b8404e0113cc5a
Mark090693 <alinashpitalnikova1993>
2020-02-20 16:29:20 +0300

    complite fifth hw

---
+++ hw5/src/by/yakovlev/Main.java
@@ -0,0 +1,20 @@
+package by.yakovlev;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int r = 7;
+        if (r == 1) {
+            System.out.println(r + " рубль");
+        }
+            else if(1 < r && r < 5)
+            {
+                System.out.println(r+ " рубля");
+            }
+            else if (r >= 5)
+            {
+                System.out.println(r + " рублей");
+             }
+    }
+}
+
