8324ea6102709a5df8a3a53e4bd7e068bda23024
Mark090693 <alinashpitalnikova1993>
2020-02-20 22:52:19 +0300

    modificated hw4

--- hw4/src/by/yakovlev/Main.java
+++ hw4/src/by/yakovlev/Main.java
@@ -3,10 +3,10 @@ package by.yakovlev;
 public class Main {
 
     public static void main(String[] args) {
-	int a = 5;
+	int a = 50;
 	int b = 8;
 	int r = 5;
-	if (r * 2 <= b)
+	if (r * 2 <= b && r *2 <=a )
         System.out.println("Круглая фигура не покроет прямоугольную дырку");
 	else
         System.out.println("Круглая фигура покроет прямоугольную дырку");
