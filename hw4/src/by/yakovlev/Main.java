8dcab4922c311aecc2c87a68046c9ce4c04deade
Mark090693 <alinashpitalnikova1993>
2020-02-21 12:03:57 +0300

    complite but not working, unfotunately hw 4

--- hw4/src/by/yakovlev/Main.java
+++ hw4/src/by/yakovlev/Main.java
@@ -3,10 +3,18 @@ package by.yakovlev;
 public class Main {
 
     public static void main(String[] args) {
-	int a = 50;
-	int b = 8;
-	int r = 5;
-	if (r * 2 <= b && r *2 <=a )
+	double a = 50;
+	double b = 8;
+	double r = 5;
+
+	double c2 = ((Math.pow((0.5 * a), 2)) + (Math.pow((0.5 * b), 2)));
+	double c = double.sqrt (double c2);
+// Вот так оно точно должно работать правильно
+// Но видимо нужно как то либо математику импортнуть ли бо еще что то
+
+
+
+	if (r >= c)
         System.out.println("Круглая фигура не покроет прямоугольную дырку");
 	else
         System.out.println("Круглая фигура покроет прямоугольную дырку");
