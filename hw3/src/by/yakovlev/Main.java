1ecf9c37d0c98e8aa5084a588e30eb6648d58ff0
Mark090693 <alinashpitalnikova1993>
2020-03-26 13:26:05 +0300

    Fixing error in TaskX

--- hw3/src/by/yakovlev/Main.java
+++ hw3/src/by/yakovlev/Main.java
@@ -1,14 +1,18 @@
 package by.yakovlev;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	int x = 345687;
-	int a;
-	a = (x % 10);
-	if (a == 7 )
-		System.out.println("Последнее число является числом 7 ");
-	else
-		System.out.println("Последнее число не является числом 7 ");
-    }
+		System.out.println("Введите число");
+    	Scanner	scanner = new Scanner(System.in);
+    	int x = scanner.nextInt();
+		int a;
+		a = (x % 10);
+		if (a == 7 )
+			System.out.println("Последнее число является числом 7 ");
+		else
+			System.out.println("Последнее число не является числом 7 ");
+		}
 }
