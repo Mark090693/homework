5b8d6fa0a262eaa8844abdd6ff03a1b643ec897f
Mark090693 <alinashpitalnikova1993>
2020-04-01 13:09:10 +0300

    Fixing error in Task 4

--- hw4/src/by/yakovlev/Main.java
+++ hw4/src/by/yakovlev/Main.java
@@ -1,22 +1,31 @@
 package by.yakovlev;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	double a = 50;
-	double b = 8;
-	double r = 5;
+		System.out.println("Введите длинну прямоугольника: ");
+		Scanner scanner = new Scanner(System.in);
+    	double a = scanner.nextInt();
+		System.out.println("Введите ширину прямоугольника: ");
+		Scanner scanner1 = new Scanner(System.in);
+		double b = scanner1.nextInt();
+		System.out.println("Введите радиус круга для проверки: ");
+		Scanner scanner2 = new Scanner(System.in);
+		int r = scanner2.nextInt();
 
-	double c2 = ((Math.pow((0.5 * a), 2)) + (Math.pow((0.5 * b), 2)));
-	double c = double.sqrt (double c2);
-// Вот так оно точно должно работать правильно
-// Но видимо нужно как то либо математику импортнуть ли бо еще что то
+		double a2 = Math.pow(a,2);
+		double b2 = Math.pow(b,2);
+
+		if (r >= (Math.sqrt(a2 + b2))/2)
+			System.out.println("Картон с радиусом " + r + " полностью закроет отверстие!");
+		else
+			System.out.println("Картон с радиусом " + r + " не закроет отверстие!");
 
 
 
-	if (r >= c)
-        System.out.println("Круглая фигура не покроет прямоугольную дырку");
-	else
-        System.out.println("Круглая фигура покроет прямоугольную дырку");
+
+
     }
 }
